0c92a950d38f599878c6f26646ff2d6bf2073c8f
Iryna Yatsenko <iryna.didova@gmail.com>
2025-02-21 23:17:52 +0200

    VimeoPlayerVideo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.getElementById("vimeo-player");
+
+const player = new Player(iframe);
+
+const STORAGE_KEY = "videoplayer-current-time";
+
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY) || 0);
+player.on("timeupdate", throttle(function (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+}, 1000));
+
+
+
 
